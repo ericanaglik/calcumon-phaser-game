e5e72c184323dd1880f9169d2732f18e4e32189e
erica2605@live.com <erica2605@live.com>
2019-12-01 18:12:45 -0800

    added treasure map and problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import Phaser from "phaser";
 import base from "./assets/Asset5.png";
 import crystal from "./assets/Asset6.png";
+import map from "./assets/Asset18.png";
 
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
+  width: 1500,
   height: 900,
   scene: {
     preload: preload,
@@ -19,11 +20,15 @@ const game = new Phaser.Game(config);
 function preload() {
   this.load.image("lock", crystal);
   this.load.image("coral", base);
+  this.load.image("map", map);
 }
 
 function create() {
   const lock = this.add.image(400, 150, "lock");
   const coral = this.add.image(400, 650, "coral");
+  const map = this.add.image(900, 400, "map");
+  
+  this.add.text(850, 370, '82 - 36', { fontSize: '50px', fill: '#000' });
 
   this.tweens.add({
     targets: lock,
